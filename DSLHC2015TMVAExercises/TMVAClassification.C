7bfa02f928ee3650a822f34746c8102b7d89bb43
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2017-01-09 23:45:40 +0100

    updating the tmva code with the appropriate cut so that it works

--- DSLHC2015TMVAExercises/TMVAClassification.C
+++ DSLHC2015TMVAExercises/TMVAClassification.C
@@ -19,37 +19,37 @@ void TMVAClassification( ) {
 
   TMVA::Factory *factory = new TMVA::Factory( "TMVA", outputFile, "!V:!Silent");
 
-  factory->AddVariable( "jtnCands", "Number of Constituents", "#", 'I' );
-  factory->AddVariable( "jtnChCands", "Number of Charged Constituents", "#", 'I' );
-  factory->AddVariable( "jtnNeCands", "Number of Neutral Constituents", "#", 'I' );
-  factory->AddVariable( "jtm", "Mass", "GeV/c2", 'F' );
-  factory->AddVariable( "jtMByPt", "Mass over pT", "1/c", 'F' );
-  factory->AddVariable( "jtRMSCand", "RMS of constituents pT", "#", 'F' );
-  factory->AddVariable( "jtAxis1", "Major axis", "#", 'F' );
-  factory->AddVariable( "jtAxis2", "Minor axis", "#", 'F' );
-  factory->AddVariable( "jtSigma", "Sigma of constituents pT", "#", 'F' );
-  factory->AddVariable( "jtR", "distribution of constituents", "#", 'F' );
-  factory->AddVariable( "jtpTD", "pTD: sum square const pT/ jet pT", "#", 'F' );
-  factory->AddVariable( "jtpull", "pull", "#", 'F' );
-  factory->AddVariable( "jtrm0p5", "radial moment (0.5)", "#", 'F' );
-  factory->AddVariable( "jtrm1", "first radial moment", "#", 'F' );
-  factory->AddVariable( "jtrm2", "second radial moment", "#", 'F' );
-  factory->AddVariable( "jtrm3", "third radial moment", "#", 'F' );
+  factory->AddVariable( "jtnCands", "Number of Constituents", "#", 'I', 0, 50);
+  factory->AddVariable( "jtnChCands", "Number of Charged Constituents", "#", 'I', 0, 50);
+  factory->AddVariable( "jtnNeCands", "Number of Neutral Constituents", "#", 'I', 0, 50);
+  factory->AddVariable( "jtm", "Mass", "GeV/c2", 'F', 0.001, 50);
+  factory->AddVariable( "jtMByPt", "Mass over pT", "1/c", 'F', 0.001, 1);
+  factory->AddVariable( "jtRMSCand", "RMS of constituents pT", "#", 'F', 0.001, 0.5);
+  factory->AddVariable( "jtAxis1", "Major axis", "#", 'F', 0.001, 0.4);
+  factory->AddVariable( "jtAxis2", "Minor axis", "#", 'F', 0.001, 0.4);
+  factory->AddVariable( "jtSigma", "Sigma of constituents pT", "#", 'F',0.001, 0.4);
+  factory->AddVariable( "jtR", "distribution of constituents", "#", 'F',0.001, 0.999);
+  factory->AddVariable( "jtpTD", "pTD: sum square const pT/ jet pT", "#", 'F', 0.001, 0.999);
+  factory->AddVariable( "jtpull", "pull", "#", 'F', 0.00001, 0.02);
+  factory->AddVariable( "jtrm0p5", "radial moment (0.5)", "#", 'F', 0.001, 1.0);
+  factory->AddVariable( "jtrm1", "first radial moment", "#", 'F', 0.001, 0.5);
+  factory->AddVariable( "jtrm2", "second radial moment", "#", 'F', 0.0001, 0.1);
+  factory->AddVariable( "jtrm3", "third radial moment", "#", 'F', 0.0001, 0.02);
   
   // read training and test data
   // load the signal and background event samples from ROOT trees
 
   TFile *fin = TFile::Open("HiForestAOD.root"); 
-   
+  
   TTree *inputTree     = (TTree*)fin->Get("ak4PFJetAnalyzer/t");
 
-  TCut sig = "subid==0 && refdrjt<0.4 && refparton_flavor>=-6 && refparton_flavor<=6 && refpt>80 && fabs(jteta)<2.0";
+  TCut sig = "subid==0 && refdrjt<0.4 && refparton_flavor>-6 && refparton_flavor<6 && refpt>80 && fabs(jteta)<2.0";
   TCut bkg = "subid==0 && refdrjt<0.4 && refparton_flavor==21 && refpt>80 && fabs(jteta)<2.0";
 
   factory->SetInputTrees(inputTree, sig, bkg);
 
   // tell the factory to use all remaining events in the trees after training for testing:
-  factory->PrepareTrainingAndTestTree( "","", "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
+  factory->PrepareTrainingAndTestTree( "subid==0 && refdrjt<0.4 && refpt>80 && fabs(jteta)<2.0", "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
 
   // factory->BookMethod( TMVA::Types::kLD, "LD", "H:!V:" );
   // factory->BookMethod( TMVA::Types::kLikelihood, "Likelihood", "H:!V:" );
