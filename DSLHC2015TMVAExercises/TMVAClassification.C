770b16f9d6527831cc5cd4efa5c108b6e5860122
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2017-01-11 15:03:57 -0500

    updated the code and location of root files

--- DSLHC2015TMVAExercises/TMVAClassification.C
+++ DSLHC2015TMVAExercises/TMVAClassification.C
@@ -13,9 +13,29 @@
 
 void TMVAClassification( ) { 
   
+  
+  // read training and test data
+  // load the signal and background event samples from ROOT trees
+  std::string infile_Forest;
+  
+  infile_Forest = "PYEPOS_pPb_pthat80_forests.txt";
+  std::ifstream instr_Forest(infile_Forest.c_str(),std::ifstream::in);
+  std::string filename_Forest;
+  
+  const int startfile = 0;
+  const int endfile = 34;
+
+  string dir = "ak4PFJetAnalyzer/t";
+  TChain * inputTree = new TChain(dir.data());
+  for(int ifile = startfile; ifile<endfile; ++ifile){
+    instr_Forest>>filename_Forest;
+    inputTree->Add(filename_Forest.c_str());
+  }
+  
   // Create a new root output file.
-  TString outfileName = "QGTest_Discriminant.root";
-  TFile*  outputFile = TFile::Open(outfileName,"RECREATE");
+  TString outfileName = "QGTest_pthat80_Discriminant.root";
+  TFile*  outputFile = new TFile(outfileName,"RECREATE");
+  outputFile->cd();
 
   TMVA::Factory *factory = new TMVA::Factory( "TMVA", outputFile, "!V:!Silent");
 
@@ -26,30 +46,26 @@ void TMVAClassification( ) {
   factory->AddVariable( "jtMByPt", "Mass over pT", "1/c", 'F', 0.001, 1);
   factory->AddVariable( "jtRMSCand", "RMS of constituents pT", "#", 'F', 0.001, 0.5);
   factory->AddVariable( "jtAxis1", "Major axis", "#", 'F', 0.001, 0.4);
-  factory->AddVariable( "jtAxis2", "Minor axis", "#", 'F', 0.001, 0.4);
-  factory->AddVariable( "jtSigma", "Sigma of constituents pT", "#", 'F',0.001, 0.4);
+  // factory->AddVariable( "jtAxis2", "Minor axis", "#", 'F', 0.001, 0.4);
+  // factory->AddVariable( "jtSigma", "Sigma of constituents pT", "#", 'F',0.001, 0.4);
   factory->AddVariable( "jtR", "distribution of constituents", "#", 'F',0.001, 0.999);
   factory->AddVariable( "jtpTD", "pTD: sum square const pT/ jet pT", "#", 'F', 0.001, 0.999);
   factory->AddVariable( "jtpull", "pull", "#", 'F', 0.00001, 0.02);
   factory->AddVariable( "jtrm0p5", "radial moment (0.5)", "#", 'F', 0.001, 1.0);
   factory->AddVariable( "jtrm1", "first radial moment", "#", 'F', 0.001, 0.5);
-  factory->AddVariable( "jtrm2", "second radial moment", "#", 'F', 0.0001, 0.1);
-  factory->AddVariable( "jtrm3", "third radial moment", "#", 'F', 0.0001, 0.02);
+  // factory->AddVariable( "jtrm2", "second radial moment", "#", 'F', 0.0001, 0.1);
+  // factory->AddVariable( "jtrm3", "third radial moment", "#", 'F', 0.0001, 0.02);
+  TCut sig = "subid==0 && refdrjt<0.4 && refparton_flavor>-6 && refparton_flavor<6 && jtpt>100 && fabs(jteta)<2.0";
+  TCut bkg = "subid==0 && refdrjt<0.4 && refparton_flavor==21 && jtpt>100 && fabs(jteta)<2.0";
+
+  // TFile *fin = TFile::Open("/mnt/hadoop/cms/store/user/rkunnawa/Pythia8_TuneCUETP8M1_170_ReggeGribovPartonMC_EposLHC_pPb_4080_4080/Pythia8_TuneCUETP8M1_170_EposLHC_pPb_8016_ReForestwNewJetVars/170110_180841/0000/HiForestAOD_MC_36.root"); 
   
-  // read training and test data
-  // load the signal and background event samples from ROOT trees
-
-  TFile *fin = TFile::Open("HiForestAOD.root"); 
+  // TTree *inputTree     = (TTree*)fin->Get("ak4PFJetAnalyzer/t");
   
-  TTree *inputTree     = (TTree*)fin->Get("ak4PFJetAnalyzer/t");
-
-  TCut sig = "subid==0 && refdrjt<0.4 && refparton_flavor>-6 && refparton_flavor<6 && refpt>80 && fabs(jteta)<2.0";
-  TCut bkg = "subid==0 && refdrjt<0.4 && refparton_flavor==21 && refpt>80 && fabs(jteta)<2.0";
-
   factory->SetInputTrees(inputTree, sig, bkg);
 
   // tell the factory to use all remaining events in the trees after training for testing:
-  factory->PrepareTrainingAndTestTree( "subid==0 && refdrjt<0.4 && refpt>80 && fabs(jteta)<2.0", "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
+  factory->PrepareTrainingAndTestTree( "subid==0 && refdrjt<0.4 && jtpt>190 && fabs(jteta)<2.0", "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
 
   // factory->BookMethod( TMVA::Types::kLD, "LD", "H:!V:" );
   // factory->BookMethod( TMVA::Types::kLikelihood, "Likelihood", "H:!V:" );
@@ -58,22 +74,18 @@ void TMVAClassification( ) {
 
   // Train MVAs using the set of training events
   factory->TrainAllMethods();
-
   // ---- Evaluate all MVAs using the set of test events
   factory->TestAllMethods();
-
   // ----- Evaluate and compare performance of all configured MVAs
   factory->EvaluateAllMethods();    
-
-  // --------------------------------------------------------------
-   
+  // --------------------------------------------------------------   
   // Save the output
   outputFile->Close();
 
-  // Launch the GUI for the root macros
-  if (!gROOT->IsBatch()) {
-    if (gROOT->GetVersionInt() > 60402) TMVA::TMVAGui( outfileName ); 
-    //     else                                {gROOT->ProcessLine(".L TMVAGui.C"); TMVAGui(outfileName);}
-  } 
+  // // Launch the GUI for the root macros
+  // if (!gROOT->IsBatch()) {
+  //   if (gROOT->GetVersionInt() > 60402) TMVA::TMVAGui( outfileName ); 
+  //   //     else                                {gROOT->ProcessLine(".L TMVAGui.C"); TMVAGui(outfileName);}
+  // } 
   delete factory;
 }
